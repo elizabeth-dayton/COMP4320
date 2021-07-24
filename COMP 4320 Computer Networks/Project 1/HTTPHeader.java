8496aa7be51c27b0020dfabfa1977d8466cd9999
elizabeth-dayton <49524477+elizabeth-dayton@users.noreply.github.com>
2021-07-23 20:27:49 -0500

    Add files via upload

---
+++ COMP 4320 Computer Networks/Project 1/HTTPHeader.java
@@ -0,0 +1,51 @@
+
+public class HTTPHeader {
+	private String mContentLength;
+	private String mContentType;
+	private int mStatusCode;
+	private int mSequenceNum;
+	private int mChecksum;
+	private final String mHTTPversion = "HTTP/1.0";
+
+
+	public HTTPHeader(String contentLength, int statusCode, String contentType, int sequenceNum, int checksum){
+		mContentLength = contentLength;
+		mStatusCode = statusCode;
+		mContentType = contentType;
+		mSequenceNum = sequenceNum;
+		mChecksum = checksum;
+	}
+
+	public String statusCodePhraseGenerator(int code){
+		switch(code){
+		case 200:
+			return "Document As Follows";
+		case 404:
+			return "File Not Found";
+		case 400:
+			return "Entered Invalid Request";
+		default:
+			return "Invalid Status Code";
+		}
+	}
+	
+
+	
+	public int getmStatusCode() {
+		return mStatusCode;
+	}
+
+	public void setmStatusCode(int mStatusCode) {
+		this.mStatusCode = mStatusCode;
+	}
+
+	public String toString(){
+		String header = mHTTPversion + " " + mStatusCode + " "+statusCodePhraseGenerator(mStatusCode) + "\r\n" +
+	"Content-Type: " +mContentType+"\r\n"+"Content-Length: " + mContentLength+"\r\n" + "Sequence Number: " 
+	+ mSequenceNum +"\r\n" + "Checksum value: " + mChecksum +"\r\n";
+		return header;
+	}
+	
+	
+	
+}
---
+++ COMP 4320 Computer Networks/Project 1/UDPClient.java
@@ -0,0 +1,218 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class UDPClient {
+	public static void main(String args[]) throws Exception {
+
+		//Reads in the keyboard stream by bytes and converts them into ASCII.
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = null;
+		byte[] sendData = new byte[1024];
+		byte[] emptyData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		String serverIP;
+		int serverPortNumber;
+		Scanner scan = new Scanner(System.in);
+		double probabilityOfDamage;
+		int checksum = 0;
+
+		while(true){		
+			System.out.print("Enter the server IP address: ");
+			serverIP = scan.nextLine();
+			try{
+				IPAddress = InetAddress.getByName(serverIP);
+			} catch(UnknownHostException e){
+				System.out.println("Invalid host name or the host name is unreachable.");
+				continue;
+			}
+			break;
+		}
+
+		System.out.print("Enter the server port number: ");
+		serverPortNumber = scan.nextInt();
+
+		System.out.print("Enter the probability of packet being damaged: ");
+		probabilityOfDamage = scan.nextDouble();
+
+		String sentence;
+		String[] request;
+		do{
+			System.out.print("Type your HTTP Request and press Enter: ");
+			sentence = "GET TestFile.html HTTP/1.0";
+			request = sentence.split("[ ]");
+		}while(request.length != 3);
+
+		System.out.print("This is the request" + request);
+		File file = new File(System.getProperty("user.dir"), "TestFile.html");
+
+		//sending the data to the server.
+		sendData = sentence.getBytes();
+
+		//before we do anything with the packet, we need to send the data through our gremlin function
+		//to potentially damage it
+		gremlinFunction(sendData, probabilityOfDamage);
+
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, serverPortNumber);
+		System.out.print(sendPacket);
+
+		System.out.println(sendPacket.getData());
+		clientSocket.send(sendPacket); 
+		scan.close();
+
+		DatagramPacket receivePacket;
+
+		receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+		clientSocket.setSoTimeout(2000);
+		try{
+		clientSocket.receive(receivePacket);
+		} catch(SocketTimeoutException e){
+			System.out.println("Too much time has gone by, connection is closed.");
+			clientSocket.close();
+		}
+		
+		if(!clientSocket.isClosed()){			
+		
+
+		String receivedPacketData = new String(receivePacket.getData());
+		System.out.print(receivedPacketData);
+		String[] receivedPacketHeaderData = receivedPacketData.split("[ ]");
+		String receivedChecksumData = receivedPacketHeaderData[10];
+		int receivedChecksum = Integer.parseInt(receivedChecksumData.replaceAll("[^0-9]",""));
+
+		if(receivedPacketHeaderData.length > 11){
+
+			//Receiving the header information
+			int indexOfcrlf = 0;
+	
+			for(int i = 0; i < 6; i++){
+				indexOfcrlf = receivedPacketData.indexOf('\n', indexOfcrlf + 1);
+		
+			}
+			String fileData = receivedPacketData.substring(indexOfcrlf + 1);
+			int eos = -1;
+			while(eos == -1){
+
+				receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+				checksum = checksumCalc(receiveData);
+
+				clientSocket.receive(receivePacket);
+				receivedPacketData = new String(receivePacket.getData());
+
+				if (checksum != receivedChecksum) {
+					System.out.println("\nThere is an error in this packet!!!\n");
+				}
+
+				System.out.println(receivedPacketData);
+				//clear the received data
+				receiveData = emptyData.clone();				
+
+				//eos - end of stream, which indicates the first occurrence of an null byte 
+				eos = receivedPacketData.indexOf(0);
+				fileData = fileData + receivedPacketData;
+			}
+			clientSocket.close();
+			Writer writer = null;
+			try {
+				fileData = fileData.trim();
+				writer = new BufferedWriter(new OutputStreamWriter(	new FileOutputStream(file), "UTF-8"));
+				writer.write(fileData);
+				writer.close();
+
+			} catch (IOException ex) {
+			} finally {
+				try {
+					writer.close();
+				} catch (Exception ex) {
+				}
+			}
+		} else {
+			file = null;
+			System.out.println("Invalid response from Server.");
+		}
+	} 
+	}
+
+
+	/**
+	 * @param probability
+	 * @return Datagrampacket to be sent to server
+	 */
+	public static byte[] gremlinFunction(byte[] sendData, double probability) {
+
+		double probOneByte = 0.5;
+		double probTwoBytes = 0.3;
+		double probThreeBytes = 0.2;
+
+		double max = 1D;
+		double min = 0D;
+		Random random = new Random();
+		double randomProbabilityDamage = random.nextDouble() * (max - min) + min;
+		double randomProbabilityBytes = random.nextDouble() * (max - min) + min;
+
+		if (randomProbabilityDamage <= probability) {
+
+			int newMax = sendData.length;
+			int newMin = 0;
+			byte[] randomByte = new byte[1];
+			int randomByteLocation;
+
+			if (randomProbabilityBytes <= probThreeBytes) {
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+				randomByte[0] = 0;
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+				randomByte[0] = 0;
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+
+			}
+
+			else if (randomProbabilityBytes <= probTwoBytes) {
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+				randomByte[0] = 0;
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+
+			}
+
+			else {
+
+				randomByteLocation = random.nextInt((newMax - newMin) + 1) + newMin;
+				random.nextBytes(randomByte);
+				sendData[randomByteLocation] = randomByte[0];
+			}
+
+		}
+
+		return sendData;
+	}
+
+	/**
+	 * @param packetData
+	 * @return int
+	 */
+	public static int checksumCalc(byte[] packetData){
+
+		int sum = 0;
+
+		for (byte b: packetData) {
+			sum += b;
+		}
+		return sum;
+	}
+
+} 
